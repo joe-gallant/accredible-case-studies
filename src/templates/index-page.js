de2ff1a06245f4a3d41d71a6f5457f8784b7c84e
Kyle Johnson <kyle.johnson@gobsmack.co.uk>
2021-02-27 16:53:52 +0000

    homepage start

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,8 +1,56 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import { graphql, navigate } from 'gatsby'
 import Layout from '../components/Layout'
+import styled from 'styled-components'
 import { BannerCarousel } from '../components/BannerCarousel'
+import { Button } from '../components/Button'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 24px;
+  padding-bottom: 24px;
+  margin-top: 60px;
+  flex-wrap: wrap;
+`
+
+const ColumnLeft = styled.div`
+  width: 45%;
+
+  @media (max-width: 767px) {
+    width: 100%;
+  }
+
+  img {
+    width: 100%;
+  }
+`
+
+const ColumnRight = styled.div`
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    margin-top: 24px;
+  }
+
+  h2 {
+    margin-bottom: 12px;
+    font-size: 48px;
+    line-height: 60px;
+    font-weight: 700;
+
+    @media (max-width: 767px) {
+      font-size: 32px;
+      line-height: 42px;
+    }
+  }
+`
 
 export const IndexPageTemplate = ({
   image,
@@ -11,6 +59,16 @@ export const IndexPageTemplate = ({
 }) => (
   <>
     <BannerCarousel title={heading} subheading={subheading}></BannerCarousel>
+    <Container className="container">
+      <ColumnLeft>
+        <img src="https://assets.website-files.com/5f68558b209a0b8f85194e47/5fdb7a8539bf2d2757ead9a2_premium_white_labelling-premium_wl_fullpage.png" />
+      </ColumnLeft>
+      <ColumnRight>
+        <h2>What is the purpose of this site?</h2>
+        <p className="text--lg">This site hosts a collection of links to digital credential case studies from a variety of different platform providers, brand names, and industry types. The case studies provide real-life applications of digital credentials and how they made a difference to the target organization. For more information, read our guide to What Is A Case Study or start browsing the library.</p>
+        <Button ClickHandler={() => navigate('/case-studies')} text="Subscribe for Updates" />
+      </ColumnRight>
+    </Container>
   </>
 )
 
