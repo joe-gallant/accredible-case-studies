2d0de3d8e3b6ce2a4bdf7bf5dab9ae045b805aa8
Kyle Johnson <kyle.johnson@gobsmack.co.uk>
2021-02-27 18:49:26 +0000

    Remove style guide

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -39,28 +39,6 @@ const AboutPage = ({ data }) => {
   return (
     <Layout>
       <Banner title={post.frontmatter.title} submitSearch={(searchTerm) => alert(searchTerm)} />
-      <div className="container">
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-        <h4>Heading 4</h4>
-        <p>lorem lorem ogierngoer ournegouern nogerunguoer nogunrg ouenrger oungr <a href="#">this is a link.</a> nrg ouenrger oung</p>
-        <p><strong>strong text</strong> lorem lorem ogierngoer ournegouern nogerunguoer nogunrg ouenrger oungr <a href="#">this is a link.</a> nrg ouenrger oung</p>
-        <ul>
-          <li>Test list item</li>
-          <li>Test list item</li>
-          <li>Test list item</li>
-          <li>Test list item</li>
-        </ul>
-        <ol>
-          <li>Test list item</li>
-          <li>Test list item</li>
-          <li>Test list item</li>
-          <li>Test list item</li>
-        </ol>
-        <Button small ClickHandler={() => navigate('/')} text="Test small" />
-        <Button ClickHandler={() => navigate('/')} text="Test normal" />
-      </div>
       <AboutPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
