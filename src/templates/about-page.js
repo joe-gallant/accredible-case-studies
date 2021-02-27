40cc99bd3f703bf7a1c168189a7694be5dc51a47
Joe Gallant <j.gallant@aitch-c.com>
2021-02-27 10:27:57 +0000

    Added style guide to about

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -36,6 +36,26 @@ const AboutPage = ({ data }) => {
 
   return (
     <Layout>
+      <div class="container">
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <p>lorem lorem ogierngoer ournegouern nogerunguoer nogunrg ouenrger oungr <a href="#">this is a link.</a> nrg ouenrger oung</p>
+        <p><strong>strong text</strong> lorem lorem ogierngoer ournegouern nogerunguoer nogunrg ouenrger oungr <a href="#">this is a link.</a> nrg ouenrger oung</p>
+        <ul>
+          <li>Test list item</li>
+          <li>Test list item</li>
+          <li>Test list item</li>
+          <li>Test list item</li>
+        </ul>
+        <ol>
+          <li>Test list item</li>
+          <li>Test list item</li>
+          <li>Test list item</li>
+          <li>Test list item</li>
+        </ol>
+      </div>
       <AboutPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
